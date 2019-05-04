db605faa0f6b8db649d1cc9e031e4df441f58f8d
Edohare <edlohare@gmail.com>
2019-05-04 11:49:23 -0400

    Making the code my own - edit 1
    Had to edit some code in the background files to make it run.

--- StarcraftAI.py
+++ StarcraftAI.py
@@ -1,15 +1,14 @@
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
+
 from sc2.player import Bot, Computer
 
-
-class SentdeBot(sc2.BotAI):
+class edbot(sc2.BotAI):
     async def on_step(self, iteration):
-        # what to do every step
-        await self.distribute_workers()  # in sc2/bot_ai.py
+        await self.distribute_workers()
 
 
 run_game(maps.get("AbyssalReefLE"), [
-    Bot(Race.Protoss, SentdeBot()),
+    Bot(Race.Protoss, edbot()),
     Computer(Race.Terran, Difficulty.Easy)
-], realtime=True)
+    ], realtime = True)
