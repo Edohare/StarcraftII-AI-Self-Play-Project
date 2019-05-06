fec0e0f42efcc3b389be17d1632b32fd2c0177b6
Edohare <edlohare@gmail.com>
2019-05-06 07:48:31 -0400

    building assimilators and expanding

--- StarcraftAI.py
+++ StarcraftAI.py
@@ -2,13 +2,15 @@ import sc2
 from sc2 import run_game, maps, Race, Difficulty
 
 from sc2.player import Bot, Computer
-from sc2.constants import NEXUS, PROBE, PYLON
+from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR
 
 class edbot(sc2.BotAI):
     async def on_step(self, iteration):
         await self.distribute_workers()
         await self.build_workers()
         await self.build_pylons()
+        await self.build_assimilators()
+        await self.expand()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready.noqueue:
@@ -22,9 +24,25 @@ class edbot(sc2.BotAI):
                 if self.can_afford(PYLON):
                     await self.build(PYLON, near=nexuses.first)
 
+    async def build_assimilators(self):
+        for nexus in self.units(NEXUS).ready:
+            vaspenes = self.state.vespene_geyser.closer_than(15.0, nexus)
+            for vaspene in vaspenes:
+                if not self.can_afford(ASSIMILATOR):
+                    break
+                worker = self.select_build_worker(vaspene.position)
+                if worker is None:
+                    break
+                if not self.units(ASSIMILATOR).closer_than(1.0, vaspene).exists:
+                    await self.do(worker.build(ASSIMILATOR, vaspene))
+                # if there isnt an assimilator there and there is a worker close, create an assimilator on a near vaspene
+
+    async def expand(self):
+        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+            await self.expand_now()
 
 
 run_game(maps.get("AbyssalReefLE"), [
     Bot(Race.Protoss, edbot()),
     Computer(Race.Terran, Difficulty.Easy)
-    ], realtime = True)
+    ], realtime = False)
